ac28eabc6e6f0905437b35d55a13220e197ea0a3
Kai Takami <kaitakami@protonmail.com>
2023-02-18 14:47:43 +0900

    Set up postgres db and github auth

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -11,11 +11,7 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-
   getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+    return "This is a protected message! Welcome to enzan :)";
   }),
 });
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,9 +4,8 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "../env.mjs";
 import { prisma } from "./db";
 
 /**
@@ -49,22 +48,12 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     **/
   ],
 };
-
 /**
  * Wrapper for `getServerSession` so that you don't need to import the
  * `authOptions` in every file.
