bdfadf37a7d1cfdee4e0ba565754247add76de13
Kai Takami <kaitakami@protonmail.com>
2023-02-19 12:11:01 +0900

    Create responsive navbar

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link"
+import { buttonVariants } from "../ui/button"
+import { siteConfig } from "@/config/siteConfig"
+import { Github } from "lucide-react"
+import { Twitter } from "lucide-react"
+import { MainNav } from "./mainNav"
+import ThemeChanger from "./ThemeChanger"
+
+const Navbar = () => {
+  return (
+    <header className="w-full top-0 z-40 absolute border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900">
+      <div className="container flex h-16 items-center m-auto px-3">
+        <MainNav items={siteConfig.mainNav} />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                  className: "text-slate-700 dark:text-slate-400",
+                })}
+              >
+                <Github className="h-5 w-5" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                  className: "text-slate-700 dark:text-slate-400",
+                })}
+              >
+                <Twitter className="h-5 w-5 fill-current" />
+                <span className="sr-only">Twitter</span>
+              </div>
+            </Link>
+            <ThemeChanger />
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
