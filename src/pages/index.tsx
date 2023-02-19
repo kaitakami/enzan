faef5c74c1a8bb2ffdf0d23a0685020d00b813b5
Kai Takami <kaitakami@protonmail.com>
2023-02-19 12:40:19 +0900

    Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import Navbar from "@/components/layout/navbar";
-
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { Button } from "@/components/ui/button";
 import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
@@ -15,7 +12,13 @@ const Home: NextPage = () => {
       <Head>
         <title>Enzan</title>
         <meta name="description" content="This website is in construction!" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Layout>
         <>
