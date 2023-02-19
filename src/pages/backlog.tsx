a8f0a18b23c502229460494c3055767a988a86f2
Kai Takami <kaitakami@protonmail.com>
2023-02-19 17:42:57 +0900

    Create backlog page

---
+++ src/pages/backlog.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/components/Layout'
+
+const backlog = () => {
+  return (
+    <Layout>
+      <div className='min-h-screen items-center text-5xl font-bold flex justify-center -mb-18 -mt-24'>
+        <h1>Backlog is under construction 🏗️</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default backlog
