c1164b1a358f1e7beef8ef01186489990ddb78a9
Kai Takami <kaitakami@protonmail.com>
2023-02-19 12:11:32 +0900

    Add next themes, inter as base font and toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,42 @@
+import "../styles/globals.css";
 import { type AppType } from "next/app";
+import { Inter } from "@next/font/google";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { ThemeProvider } from "next-themes"
 import { api } from "../utils/api";
+import { Toaster } from "@/components/ui/toaster"
 
-import "../styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+				:root {
+					--font-inter: ${inter.style.fontFamily};
+				}
+			}`}</style>
+      <SessionProvider session={session}>
+        <ThemeProvider>
+          <div
+            className={`${inter.variable} font-sans selection:bg-fuchsia-400 selection:text-white min-h-screen relative bg-background text-foreground dark:bg-foreground dark:text-background`}
+          >
+            <Component {...pageProps} />
+            <Toaster />
+          </div>
+        </ThemeProvider>
+      </SessionProvider>
+    </>
+
   );
 };
 
