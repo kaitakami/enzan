5a8504ada94c5f8b5f91148f5c9b10d545f21b0f
Kai Takami <kaitakami@protonmail.com>
2023-02-19 17:42:49 +0900

    Modify and add new data to siteConfig

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -3,16 +3,20 @@ import { NavItem } from "@/types/nav";
 interface SiteConfig {
   name: string;
   description: string;
+  secondDescription: string;
   mainNav: NavItem[];
   links: {
     twitter: string;
     github: string;
+    contact: string;
+    blog: string;
   };
 }
 
 export const siteConfig: SiteConfig = {
   name: "Enzan",
   description: "Colabora con otros developers y crea tu portfolio ideal",
+  secondDescription: "Juntamos a personas que quieren crear tech.",
   mainNav: [
     {
       title: "Inicio",
@@ -20,7 +24,9 @@ export const siteConfig: SiteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/kaitakami",
+    twitter: "https://twitter.com/kaitakami_",
     github: "https://github.com/kaitakami/enzan",
+    contact: "https://www.kaitakami.dev#contact",
+    blog: "https://www.kaitakami.dev/blog",
   },
 };
