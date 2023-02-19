2bfdbbbfb1f13aadd4b8a011c1442410f5ae1eed
Kai Takami <kaitakami@protonmail.com>
2023-02-19 12:08:12 +0900

    Add inter font to tailwindcss config and extend theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,61 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class", '[data-theme="dark"]'],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        background: "#fafafa",
+        foreground: "#222222",
+        neutral: {
+          50: "#fafafa",
+          100: "#f1f1f1",
+          200: "#e0e0e0",
+          300: "#cbcbcb",
+          400: "#999999",
+          500: "#6a6a6a",
+          600: "#4c4c4c",
+          700: "#393939",
+          800: "#222222",
+          900: "#181818",
+        },
+        emerald: {
+          DEFAULT: "#34d399",
+          50: "#ecfdf5",
+          100: "#d1fae5",
+          200: "#a7f3d0",
+          300: "#6ee7b7",
+          400: "#34d399",
+          500: "#10b981",
+          600: "#059669",
+          700: "#047857",
+          800: "#065f46",
+          900: "#064e3b",
+        },
+      },
+    },
   },
-  plugins: [],
-};
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  plugins: [require("tailwindcss-animate")],
+}
