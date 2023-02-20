8a1860e91f0572c2672f2a8469a39f04d7f92606
Kai Takami <kaitakami@protonmail.com>
2023-02-20 22:31:32 +0900

    Add toast for sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,67 @@
 import { type NextPage } from "next";
-import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
-
+import { buttonVariants } from "@/components/ui/button";
 import Layout from '../components/Layout';
 import { siteConfig } from '../config/siteConfig';
 import { Github } from "lucide-react";
+import { useToast } from "@/hooks/use-toast"
 import ProjectsList from "@/components/Home/ProjectsList";
 import FAQ from "@/components/Home/FAQ";
+import { ToastAction } from "@radix-ui/react-toast";
+
 
 const Home: NextPage = () => {
-
+  const { toast } = useToast()
   return (
     <>
       <Layout>
         <>
-          <section className="bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-[#f1f1f1] via-slate-100 to-white dark:from-slate-900 dark:via-slate-700 dark:to-slate-800 min-h-screen sm:pt-28 pt-10">
+          <section className="from-[#f1f1f1] via-slate-100 to-[#fafafa] dark:from-slate-900 dark:via-slate-800 dark:to-slate-800 h-screen sm:pt-16 pt-5 bg-gradient-to-b max-h-[1200px] relative">
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
               <Link href="/backlog" className="group inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                 <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3 group-hover:animate-pulse">Nuevo</span> <span className="text-sm font-medium">Enzan ya está aquí! Mira las novedades</span>
                 <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
               </Link>
               <div className="space-y-5">
-                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white max-w-xl m-auto">{siteConfig.secondDescription}</h1>
-                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{siteConfig.description}</p>
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none bg-gradient-to-b from-slate-900 via-slate-800 to-slate-800 md:text-7xl lg:text-8xl bg-clip-text text-transparent dark:text-slate-100 sm:max-w-xs sm:px-0 md:max-w-2xl  lg:max-w-4xl py-8 m-auto backdrop-blur-3xl">{siteConfig.secondDescription}</h1>
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400 backdrop-blur-xl">{siteConfig.description}</p>
                 <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                  <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                  <Link href="/about" className={buttonVariants({
+                    size: "lg",
+                    variant: "colored",
+                    className: "py-6"
+                  })}>
                     Sobre
                     <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path></svg>
-                  </a>
-                  <Link href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 space-x-1">
+                  </Link>
+                  <button
+                    className={buttonVariants({
+                      size: "lg",
+                      variant: "outline",
+                      className: "bg-white dark:bg-inherit py-6 space-x-2"
+                    })}
+                    onClick={async () => {
+                      try {
+                        toast({
+                          title: "Cargando...",
+                          description: "Estamos conectando tu cuenta de GitHub",
+                        })
+                        await signIn("github")
+                      }
+                      catch {
+                        toast({
+                          variant: "destructive",
+                          title: "Uh oh!",
+                          description: "Hubo un error :(",
+                          action: <ToastAction altText="Reintentar">Reintentar</ToastAction>
+                        })
+                      }
+                    }}
+                  >
                     <Github />
                     <span>Registra tu cuenta</span>
-                  </Link>
+                  </button>
                 </div>
               </div>
             </div>
