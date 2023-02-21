474e634b8be48c732cb49c557677234b18ce70ac
Kai Takami <kaitakami@protonmail.com>
2023-02-21 23:46:19 +0900

    Create Head Layout

---
+++ src/components/Dashboard/layout/HeadLayout.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import React from 'react'
+
+const HeadLayout = (
+  {
+    title = "Enzan - Crea junto a otros developers",
+    description = "Juntamos a personas que quieren crear tech. Colabora con otros developers y crea tu portfolio ideal o tu siguente gran proyecto."
+  }
+) => {
+  return (
+    <Head>
+      <meta name="author" content="Kai Takami" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+      <link rel="manifest" href="/images/site.webmanifest" />
+      <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+      {/* <!-- Primary Meta Tags --> */}
+      <title>{title}</title>
+      <meta name="title" content="Enzan - Crea junto a otros developers" />
+      <meta name="description" content={description} />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="Enzan - Crea junto a otros developers" />
+      <meta property="og:description" content="Juntamos a personas que quieren crear tech.
+Colabora con otros developers y crea tu portfolio ideal o tu siguente gran proyecto." />
+      <meta property="og:image" content="https://ibb.co/L5J8L2c" />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="Enzan - Crea junto a otros developers" />
+      <meta property="twitter:description" content="Juntamos a personas que quieren crear tech.
+Colabora con otros developers y crea tu portfolio ideal o tu siguente gran proyecto." />
+      <meta property="twitter:image" content="https://ibb.co/L5J8L2c" />
+    </Head>
+  )
+}
+
+export default HeadLayout
