9a6b137b1e43f9f1d113fd93d3a24b1a5759a3ee
Kai Takami <kaitakami@protonmail.com>
2023-02-22 13:16:19 +0900

    Put meta tags first before links in head

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -10,15 +10,6 @@ const HeadLayout = (
   return (
     <>
       <Head>
-        <meta name="author" content="Kai Takami" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
-        <link rel="manifest" href="/images/site.webmanifest" />
-        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-        {/* <!-- Primary Meta Tags --> */}
         <title>{title}</title>
         <meta name="title" content="Enzan - Crea junto a otros developers" />
         <meta name="description" content={description} />
@@ -38,6 +29,15 @@ Colabora con otros developers y crea tu portfolio ideal o tu siguente gran proye
         <meta property="twitter:description" content="Juntamos a personas que quieren crear tech.
 Colabora con otros developers y crea tu portfolio ideal o tu siguente gran proyecto." />
         <meta property="twitter:image" content="https://i.ibb.co/VmQt7Pz/Frame-346.png" />
+        <meta name="author" content="Kai Takami" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+        {/* <!-- Primary Meta Tags --> */}
       </Head>
       {/* Umami */}
       <Script async defer data-website-id="3b1eed1e-0265-4b9b-9594-db63aa78cd5d" src="https://umami.kaitakami.dev/umami.js" />
