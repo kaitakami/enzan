11d0ba8f29b0707879cee3359010566a9b04112c
Kai Takami <kaitakami@protonmail.com>
2023-02-23 20:30:50 +0900

    Add tailwind typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -57,5 +57,5 @@ module.exports = {
   },
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')],
 }
