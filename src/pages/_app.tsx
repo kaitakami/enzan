3f1f9b7aaf16204a6dff7d50a3d97fbcba707882
Kai Takami <kaitakami@protonmail.com>
2023-02-25 23:40:45 +0900

    Use next/font instead of @next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { type AppType } from "next/app";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes"
