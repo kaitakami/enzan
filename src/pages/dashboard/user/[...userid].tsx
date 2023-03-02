0cbf63ab7daac46fb68b893f3eaf16585e805c3f
Kai Takami <kaitakami@protonmail.com>
2023-03-02 20:38:34 +0900

    Create dynamic routing

---
+++ src/pages/dashboard/user/[...userid].tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Profile = () => {
+  // TODO: Create different profile page for public and private (authenticated) user
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
