337efdb71dda81c3e590d457ec47168223dfcd20
Kai Takami <kaitakami@protonmail.com>
2023-03-02 20:37:26 +0900

    Add toast when signing out

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,14 +2,22 @@ import Layout from "@/components/layout/Dashboard/Layout"
 import { Button } from "@/components/ui/button"
 import type { NextPage } from "next"
 import { useSession, signOut } from "next-auth/react"
+import { useToast } from "@/hooks/use-toast"
 
 const Dashboard: NextPage = () => {
   const { data: session } = useSession()
+  const { toast } = useToast()
+
   return (
     <Layout>
       <div className="">
         <h1>Dashboard</h1>
-        <Button variant={'destructive'} onClick={() => { signOut().catch((err) => console.log(err)) }}>Sign Out</Button>
+        <Button variant={'destructive'} onClick={() => {
+          signOut().catch(() => toast({
+            variant: "destructive",
+            title: "Error cerrando sesión",
+          }))
+        }}>Cerrar sesión</Button>
         <h2>Welcome back! {session?.user.name}</h2>
       </div>
     </Layout>
