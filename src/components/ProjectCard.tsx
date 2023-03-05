99e70d00fa21fc6e20fa4cded3d705d30d530b8a
Kai Takami <kaitakami@protonmail.com>
2023-03-05 20:15:48 +0900

    Make project card into component

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import { convertDuration } from '../utils/cleanDuration';
+import type { Project } from "@prisma/client";
+import type { Language } from "@prisma/client";
+
+interface ProjectWithLanguage extends Project {
+  languages: Language[]
+}
+
+const ProjectCard: React.FC<{ project: ProjectWithLanguage }> = ({ project }) => {
+  const { slug, name, duration, languages, tags, description } = project
+  return (
+    <Link key={slug} href={`/dashboard/projects/${slug}`} className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+      <div className="flex flex-wrap gap-2">
+        <span className="bg-blue-100 text-blue-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{convertDuration(duration)}</span>
+        {languages.map(({ name }) => (
+          <span key={name} className="bg-primary-200 text-primary-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-primary-800 dark:text-primary-300">{name}</span>
+        ))}
+        {tags.map((tag) => (
+          <span key={tag} className="bg-primary-100 text-primary-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-primary-900 dark:text-primary-300">{tag}</span>
+        ))}
+      </div>
+      <p className="font-normal text-gray-700 dark:text-gray-400">{`${description.split(" ").slice(0, 23).join(" ")}${description.split(" ").length > 23 ? "..." : ""}`}</p>
+    </Link>
+  )
+}
+
+export default ProjectCard
--- src/components/layout/Dashboard/User/Profile.tsx
+++ src/components/layout/Dashboard/User/Profile.tsx
@@ -1,47 +1,71 @@
+import { useState } from "react"
 import type { UserWithProjectsAndAdmissions } from "@/pages/dashboard/user/[...userId]"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
+import Project from "@/components/ProjectCard"
+import { Button } from "@/components/ui/button"
 
 const Profile: React.FC<{ user: UserWithProjectsAndAdmissions }> = ({ user }) => {
+  const [showMore, setShowMore] = useState({
+    projects: 3,
+    admissions: 3,
+  })
+  const handleShowMore = (type: string) => {
+    if (type === 'projects') {
+      setShowMore({
+        ...showMore,
+        projects: showMore.projects + 3,
+      })
+    }
+  }
   return (
     <>
-      <section className="p-3 max-w-6xl w-screen space-y-7 rounded">
+      <section className="p-3 max-w-6xl w-screen space-y-8 rounded">
         <div className="flex flex-wrap gap-8">
           <Avatar className="w-24 h-24 md:w-36 md:h-36">
             <AvatarImage src={user.image || ""} />
-            <AvatarFallback>{user.name?.slice(0, 2)}</AvatarFallback>
+            <AvatarFallback>{user.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
           <h1 className="my-auto scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{user.name}</h1>
         </div>
-        <div className="flex justify-between max-w-4xl dark:bg-slate-900 p-5 mx-auto">
-          <div className="flex flex-col justify-center text-center">
-            <h3>
+        <p className="max-w-4xl dark:text-slate-200 text-slate-700">
+          {/* TODO: migrate db, add description */}
+        </p>
+        <hr />
+        <div className="flex flex-wrap md:grid md:grid-cols-3 p-5 mx-auto gap-5">
+          <div className="flex flex-col justify-center text-center px-14 py-10 space-y-2 dark:bg-slate-900 bg-slate-200 rounded-md  shadow-xl max-w-xs w-full mx-auto">
+            <h3 className="text-xl font-semibold">
               Projectos
             </h3>
-            <span>{user.projects.length}</span>
+            <span className="text-2xl font-extrabold">{user.projects.length}</span>
           </div>
-          <div className="flex flex-col justify-center text-center">
-            <h3>
-              Puntos
+          <div className="flex flex-col justify-center text-center px-14 py-10 space-y-2 dark:bg-slate-900 bg-slate-200 rounded-md shadow-xl max-w-xs w-full mx-auto">
+            <h3 className="text-xl font-semibold">
+              Puntos 🎉
             </h3>
-            <span>{user.points}</span>
+            <span className="text-2xl font-extrabold">{user.points}</span>
           </div>
-          <div className="flex flex-col justify-center text-center">
-            <h3>
+          <div className="flex flex-col justify-center text-center px-14 py-10 space-y-2 dark:bg-slate-900 bg-slate-200 rounded-md shadow-xl max-w-xs w-full mx-auto">
+            <h3 className="text-xl font-semibold">
               {/* User can make an update every day for each project */}
               Updates
             </h3>
-            <span>
+            <span className="text-2xl font-extrabold">
               {/* User updates */}
               3</span>
           </div>
         </div>
-        <div>
-          {user.projects.map(project => (
-            <div key={project.slug}>
-              {project.name}
-            </div>
-          ))}
+        <div className="space-y-4">
+          <h2 className="mt-10 scroll-m-20 border-b border-b-slate-200 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 dark:border-b-slate-700">Projectos</h2>
+          <div className="flex flex-wrap md:grid md:grid-cols-2 gap-5">
+            {user.projects.slice(0, showMore.projects).map(project => (
+              <Project key={project.slug} project={project} />
+            ))}
+          </div>
+          {showMore.projects < user.projects.length &&
+            (<div>
+              <Button variant="outline" className="block mx-auto" onClick={() => handleShowMore('projects')}>Mostrar más</Button>
+            </div>)
+          }
           {user.admissions.map(admission => (
             <div key={admission.createdAt.toString()}>
               {admission.projectId}
