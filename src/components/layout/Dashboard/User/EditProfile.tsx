20871dfa83b653c70a33861062280b5605e7838b
Kai Takami <kaitakami@protonmail.com>
2023-03-09 21:18:44 +0900

    Create Dialog to edit profile

--- src/components/layout/Dashboard/User/EditProfile.tsx
+++ src/components/layout/Dashboard/User/EditProfile.tsx
@@ -1,5 +1,7 @@
 "use client"
-
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { useToast } from "@/hooks/use-toast"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -14,7 +16,30 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 
+import { api } from "@/utils/api"
+
+interface MutationResult {
+  success: boolean
+}
+
 const EditProfile = () => {
+  const [updatedInfo, setUpdatedInfo] = useState({ name: "", description: "" })
+  const { toast } = useToast()
+  const router = useRouter()
+  const userUpdateMutation = api.user["edit-profile"].useMutation()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUpdatedInfo({ ...updatedInfo, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = () => {
+    if (updatedInfo.name === "" && updatedInfo.description === "") {
+      toast({ variant: "destructive", title: "Error", description: "No puedes guardar cambios sin cambiar ningún campo" })
+    } else {
+      userUpdateMutation.mutate(updatedInfo)
+      router.reload()
+    }
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -32,17 +57,17 @@ const EditProfile = () => {
             <Label htmlFor="name" className="text-right">
               Nombre
             </Label>
-            <Input id="name" value="" className="col-span-3" />
+            <Input id="name" name="name" value={updatedInfo.name} className="col-span-3" onChange={handleChange} />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="description" className="text-right">
               Descripción
             </Label>
-            <Textarea id="description" value="" className="col-span-3" maxLength={500} />
+            <Textarea id="description" name="description" value={updatedInfo.description} className="col-span-3" maxLength={500} onChange={handleChange} />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Guardar cambios</Button>
+          <Button type="submit" onClick={handleSubmit}>Guardar cambios</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
