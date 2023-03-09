5e0a3dc5f158ea32e050342b0f2a9d6db3165cdd
Kai Takami <kaitakami@protonmail.com>
2023-03-09 21:20:41 +0900

    Add mutation to edit user

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,23 +1,32 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import type { Prisma } from "@prisma/client";
 
 export const userRouter = createTRPCRouter({
-  ["getUnique"]: publicProcedure
+  ["edit-profile"]: protectedProcedure
     .input(
       z.object({
-        id: z.string(),
+        name: z.string(),
+        description: z.string().max(500),
       })
     )
-    .query(async ({ ctx, input }) => {
-      const user = await ctx.prisma.user.findUnique({
+    .mutation(async ({ ctx, input }) => {
+      const data: Prisma.UserUpdateInput = {};
+
+      if (input.name) {
+        data.name = input.name;
+      }
+
+      if (input.description) {
+        data.description = input.description;
+      }
+
+      await ctx.prisma.user.update({
         where: {
-          id: input.id,
+          id: ctx.session.user.id,
         },
-        include: {
-          projects: true,
-        },
+        data: data,
       });
-      return user;
     }),
 });
