28aa1eed09b6370504cfe5be3987f06a9c7f2735
Kai Takami <kaitakami@protonmail.com>
2023-03-10 23:06:16 +0900

    Create accept and reject procedure for admission

--- src/server/api/routers/admissions.ts
+++ src/server/api/routers/admissions.ts
@@ -7,7 +7,7 @@ export const admissionRouter = createTRPCRouter({
     .input(
       z.object({
         message: z.string(),
-        projectId: z.string()
+        projectId: z.string(),
       })
     )
     .mutation(async ({ ctx, input }) => {
@@ -16,7 +16,48 @@ export const admissionRouter = createTRPCRouter({
           content: input.message,
           projectId: input.projectId,
           userId: ctx.session.user.id,
-        }
+        },
+      });
+    }),
+  ["accept"]: protectedProcedure
+    .input(
+      z.object({
+        admissionId: z.string(),
+        userId: z.string(),
+        projectId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.project.update({
+        where: {
+          id: input.projectId,
+        },
+        data: {
+          members: {
+            connect: {
+              id: input.userId,
+            },
+          },
+        },
+      });
+
+      await ctx.prisma.admission.delete({
+        where: {
+          id: input.admissionId,
+        },
+      });
+    }),
+  ["reject"]: protectedProcedure
+    .input(
+      z.object({
+        admissionId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.admission.delete({
+        where: {
+          id: input.admissionId,
+        },
       });
     }),
 });
