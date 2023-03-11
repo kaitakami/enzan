c27406e2e636b2eb41efe3ba2134ebecdd19c9bc
Kai Takami <kaitakami@protonmail.com>
2023-03-11 21:56:21 +0900

    Add two parameters (name and placeholder)

--- src/components/dashboard/MDX/MdxEditor.tsx
+++ src/components/dashboard/MDX/MdxEditor.tsx
@@ -3,7 +3,7 @@ import MdxPreview from './MdxPreview';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 
-const MdxEditor: React.FC<{ state: string, handleState: (e: React.ChangeEvent<HTMLTextAreaElement>) => void }> = ({ state, handleState }) => {
+const MdxEditor: React.FC<{ state: string, handleState: (e: React.ChangeEvent<HTMLTextAreaElement>) => void, name: string, placeholder: string }> = ({ state, handleState, name, placeholder }) => {
   return (
     <div>
       <Tabs defaultValue="editor" className="dark:bg-slate-800 min-h-[400px] rounded-lg p-2 bg-slate-100 mx-auto">
@@ -12,7 +12,7 @@ const MdxEditor: React.FC<{ state: string, handleState: (e: React.ChangeEvent<HT
           <TabsTrigger value="preview">Previa</TabsTrigger>
         </TabsList>
         <TabsContent value="editor" className='h-[400px] overflow-y-auto'>
-          <TextEditor value={state} handleChange={handleState} />
+          <TextEditor value={state} handleChange={handleState} name={name} placeholder={placeholder} />
         </TabsContent>
         <TabsContent value="preview" className='h-[400px] overflow-y-auto'>
           <MdxPreview value={state} />
--- src/components/dashboard/MDX/TextEditor.tsx
+++ src/components/dashboard/MDX/TextEditor.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Textarea } from "@/components/ui/textarea"
 
-const TextEditor: React.FC<{ value: string, handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void }> = ({ value, handleChange }) => {
+const TextEditor: React.FC<{ value: string, handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void, name: string, placeholder: string }> = ({ value, handleChange, name, placeholder }) => {
   return (
-    <Textarea className='h-[350px]' placeholder="Escribe una descripción de tu proyecto en sintaxis MDX o texto plano" name='description' value={value} onChange={(e) => handleChange(e)} required />
+    <Textarea className='h-[350px]' placeholder={placeholder} name={name} value={value} onChange={(e) => handleChange(e)} required />
   )
 }
 
