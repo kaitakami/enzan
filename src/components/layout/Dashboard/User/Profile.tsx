a66d8d016b8e582cad775743d81c92d9cfed3e62
Kai Takami <kaitakami@protonmail.com>
2023-03-12 13:22:36 +0900

    Add user github in profile

--- src/components/layout/Dashboard/User/Profile.tsx
+++ src/components/layout/Dashboard/User/Profile.tsx
@@ -4,6 +4,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Project from "@/components/ProjectCard"
 import { Button } from "@/components/ui/button"
 import UpdateCollapsible from "@/components/dashboard/UpdateCollapsible"
+import { Github } from "lucide-react"
+import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button"
 
 const Profile: React.FC<{ user: UserWithProjects }> = ({ user }) => {
   const [showMore, setShowMore] = useState({
@@ -18,6 +21,7 @@ const Profile: React.FC<{ user: UserWithProjects }> = ({ user }) => {
       })
     }
   }
+
   return (
     <>
       <section className="max-w-6xl space-y-8 rounded mx-auto">
@@ -27,6 +31,11 @@ const Profile: React.FC<{ user: UserWithProjects }> = ({ user }) => {
             <AvatarFallback>{user.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
           <h1 className="my-auto scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{user.name}</h1>
+          {user.githubURL && (
+            <Link href={user.githubURL} className={buttonVariants({ className: "gap-2 my-auto" })} target="_blank">
+              <Github />
+            </Link>
+          )}
         </div>
         <p className="max-w-4xl dark:text-slate-200 text-slate-700">
           {user.description}
@@ -55,6 +64,18 @@ const Profile: React.FC<{ user: UserWithProjects }> = ({ user }) => {
         </div>
         <div className="space-y-4">
           <h2 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">Proyectos</h2>
+          {user.projects.length === 0 && (
+            <article
+              className="animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-0.5 shadow-xl transition [animation-duration:_6s] dark:shadow-gray-700/25 max-w-md"
+            >
+              <div className="rounded-[10px] bg-white dark:bg-gray-900 sm:p-6">
+                <h3 className="mt-0.5 text-lg font-medium text-gray-900 dark:text-white">
+                  No hay proyectos aún
+                </h3>
+              </div>
+            </article>
+
+          )}
           <div className="flex flex-wrap md:grid md:grid-cols-2 gap-5">
             {user.projects.slice(0, showMore.projects).map(project => (
               <Project key={project.slug} project={project} />
