684e5831d13e241e2faa7dcace21f02278f63477
Kai Takami <kaitakami@protonmail.com>
2023-03-12 13:22:22 +0900

    Add github url input and validateURL function

--- src/components/layout/Dashboard/User/EditProfile.tsx
+++ src/components/layout/Dashboard/User/EditProfile.tsx
@@ -18,12 +18,18 @@ import { Textarea } from "@/components/ui/textarea"
 
 import { api } from "@/utils/api"
 
-interface MutationResult {
-  success: boolean
+function validURL(str: string) {
+  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
 }
 
 const EditProfile = () => {
-  const [updatedInfo, setUpdatedInfo] = useState({ name: "", description: "" })
+  const [updatedInfo, setUpdatedInfo] = useState({ name: "", description: "", githubURL: "" })
   const { toast } = useToast()
   const router = useRouter()
   const userUpdateMutation = api.user["edit-profile"].useMutation()
@@ -33,9 +39,27 @@ const EditProfile = () => {
   }
 
   const handleSubmit = () => {
-    if (updatedInfo.name === "" && updatedInfo.description === "") {
-      toast({ variant: "destructive", title: "Error", description: "No puedes guardar cambios sin cambiar ningún campo" })
+    if (updatedInfo.name === "" && updatedInfo.description === "" && updatedInfo.githubURL === "") {
+      toast({ variant: "destructive", title: "Error", description: "No puedes guardar cambios sin llenar ningún campo" })
     } else {
+      if (updatedInfo.githubURL) {
+        if (validURL(updatedInfo.githubURL)) {
+          const url = new URL(updatedInfo.githubURL)
+          if (url.hostname !== "github.com") {
+            toast({ variant: "destructive", title: "Error", description: "La URL de GitHub no es válida" })
+            return
+          }
+        } else {
+          toast({ variant: "destructive", title: "Error", description: "La URL de GitHub no es válida" })
+          return
+        }
+      }
+
+      if (updatedInfo.description.length > 500) {
+        toast({ variant: "destructive", title: "Error", description: "La descripción no puede ser mayor a 500 caracteres" })
+        return
+      }
+
       userUpdateMutation.mutate(updatedInfo)
       router.reload()
     }
@@ -53,13 +77,19 @@ const EditProfile = () => {
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
+          <div>
             <Label htmlFor="name" className="text-right">
               Nombre
             </Label>
             <Input id="name" name="name" value={updatedInfo.name} className="col-span-3" onChange={handleChange} />
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
+          <div>
+            <Label htmlFor="githubURL" className="text-right">
+              La URL de tu perfil de GitHub
+            </Label>
+            <Input type="url" id="githubURL" name="githubURL" value={updatedInfo.githubURL} className="col-span-3" onChange={handleChange} />
+          </div>
+          <div>
             <Label htmlFor="description" className="text-right">
               Descripción
             </Label>
