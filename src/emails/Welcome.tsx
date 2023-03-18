1c4c7b8b4a53e454bea440440a8b6167aeb21624
Kai Takami <kaitakami@protonmail.com>
2023-03-18 22:42:19 +0900

    Create welcome email template

--- src/emails/Welcome.tsx
+++ src/emails/Welcome.tsx
@@ -1,29 +1,29 @@
 import { Html } from '@react-email/html'
-import { Head } from '@react-email/head';
 import { Tailwind } from '@react-email/tailwind';
-
+import { Container } from '@react-email/container';
+import { Hr } from '@react-email/hr'
 
 const WelcomeEmail = ({ name }: { name: string }) => {
   return (
     <Html lang='es'>
-      <Head>
-        <title>{name} bienvenid@ a Enzan ✨</title>
-      </Head>
       <body>
-        <div>
+        <Container>
           <Tailwind>
             <h1 className='text-lg font-bold'>{name} bienvenid@ a Enzan ✨</h1>
-            <p>Muchas gracias por unirte. Soy Kai, el fundador de Enzan, y quiero darte una cálida bienvenida a
+            <p>Muchas gracias por unirte. Soy Kai, el fundador de Enzan, y quiero darte una cálida bienvenida a{" "}
               <a href="https://dub.enzan.dev/comunidad">nuestra comunidad</a>.
             </p>
-            <p>Enzan es una plataforma para crear proyectos y colaborar con otros developers 💻. Estoy trabajando en mejorar la plataforma para proporcionarte una experiencia única, así que no dudes en compartirme tus preguntas y/o sugerencias. Si gustas puedes <a href="https://dub.enzan.dev/feedback">agendar una videollamada conmigo</a></p>
-            <p>Tu opinión es muy importante, Enzan está en fase beta haciendo mejoras.</p>
+            <p>Enzan es una plataforma para crear proyectos y colaborar con otros developers 💻. Estoy trabajando en mejorar la plataforma para proporcionarte una experiencia única, así que no dudes en compartirme tus preguntas y/o sugerencias. Si gustas puedes <a href="https://dub.enzan.dev/feedback">agendar una videollamada conmigo</a>.</p>
+            <p>Tu opinión es muy importante, Enzan está en fase beta haciendo mejoras todos los días. También es open source por lo que puedes ver el{" "}<a href="https://dub.enzan.dev/github">código base en GitHub.</a></p>
+            <p>Si quieres ver las futuras actualizaciones puedes ver el {" "}<a href="https://dub.enzan.dev/roadmap">roadmap</a>.</p>
             <p>Gracias por ser parte de Enzan.</p>
-            <p>Saludos cordiales, <br />
-              <span className='italic'>Kai 😊</span>
+            <p className='italic'>Saludos cordiales, <br />
+              Kai Takami ⚛️
             </p>
+            <Hr />
+            <a href="https://dub.enzan.dev/unsubscribe" className="underline opacity-40 text-sm py-6">Dejar de recibir emails</a>
           </Tailwind>
-        </div>
+        </Container>
       </body>
     </Html>
   );
